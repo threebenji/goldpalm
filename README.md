550ce64dc18f2da63d37a920a9067da2c5172f4f
three <three.benji@gmail.com>
2017-03-02 00:59:22 +0800

    a

--- README.md
+++ README.md
@@ -127,3 +127,76 @@
             ]
     
         ]);
+        
+ ##方法
+ 
+    /**
+     * 上传电子合同，上传后进入已提交状态 or 上传后不提交
+     * @param $contract
+     * @param $is_upload bool true 上传后不提交 false 上传后提交
+     * @return mixed
+     * @throws \Exception
+     */
+    public function submit($contract, $is_upload = false)
+        
+    /**
+     * 提交已经上传的合同
+     * @param $id
+     * @param $no
+     * @return bool
+     * @throws \Exception
+     */
+    public function submitStatus($id, $no)
+    
+    /**
+     * 取消合同
+     * @param $id
+     * @param $no
+     * @return bool
+     * @throws \Exception
+     */
+    public function cancelContract($id, $no)
+    
+    /**
+     * 补充完善合同保险信息接口
+     * @param $no
+     * @param $insurance string json
+     * @return bool
+     * @throws \Exception
+     */
+    public function complementInsurance($no, $insurance)
+    
+    /**
+     * 补充游客列表只有为提交的合同可以使用
+     * @param $guests
+     * @return bool
+     * @throws \Exception
+     */
+    public function complementGuest($guests)
+    
+    /**
+     * 补充签名功能
+     * @param $id
+     * @param $base64image
+     * @return bool
+     * @throws \Exception
+     */
+    public function submitSign($id,$base64image)
+    
+    /**
+     * 根据合同ID获取是否签名
+     * @param $id
+     * @return bool|\Exception|\SoapFault
+     * @throws \Exception
+     */
+    public function getSignCreate($id)
+    
+    /**
+     * 重发短信
+     * @param $id
+     * @param $guestname
+     * @param $guestmobile
+     * @return bool
+     * @throws \Exception
+     */
+    public function resendMsg($id,$guestname,$guestmobile)
--- src/goldpalm.php
+++ src/goldpalm.php
@@ -10,11 +10,18 @@ class GoldPalm
 {
 
     private $api_url = [
-        'test'=>"https://test-api.lyht.cn/ntsms-contract-api/service/ContractWebService?wsdl",
-        'production'=>"https://api.lyht.cn/ntsms-contract-api/service/ContractWebService?wsdl"
+        'test' => "https://test-api.lyht.cn/ntsms-contract-api/service/ContractWebService?wsdl",
+        'production' => "https://api.lyht.cn/ntsms-contract-api/service/ContractWebService?wsdl"
     ];
     private $api;
     private $token;
+
+    /**
+     * 实例化基础配置
+     * GoldPalm constructor.
+     * @param $config
+     * @throws \Exception
+     */
     public function __construct($config)
     {
 
@@ -23,10 +30,10 @@ class GoldPalm
             $this->api = $this->api_url[$config['env'] ? $config['env'] : 'test'];
             $client = new \SoapClient($this->api);
             //默认先取authcode为登录token
-            if(isset($config['authcode']) && empty($config['authcode'])){
+            if (isset($config['authcode']) && empty($config['authcode'])) {
                 $param = ['code' => $config['username'], 'authcode' => $config['authcode']];
                 $res = $client->__call('authenticationOTA', ['param' => $param]);
-            }else{
+            } else {
                 $param = ['code' => $config['username'], 'password' => md5($config['password'])];
                 $res = $client->__call('authentication', ['param' => $param]);
             }
@@ -34,7 +41,7 @@ class GoldPalm
 
             if ($result->result == 'success') {
                 $this->token = $result->token;
-            }else{
+            } else {
                 throw  new \Exception(json_encode($result->errors));
             }
 
@@ -44,12 +51,13 @@ class GoldPalm
     }
 
     /**
-     * 提交合同
+     * 上传电子合同，上传后进入已提交状态 or 上传后不提交
      * @param $contract
-     * @return bool
+     * @param $is_upload bool true 上传后不提交 false 上传后提交
+     * @return mixed
      * @throws \Exception
      */
-    public function submit($contract)
+    public function submit($contract, $is_upload = false)
     {
         try {
             $submitRequest = array();
@@ -92,18 +100,18 @@ class GoldPalm
             $contractTeam['enddate'] = $contract['end_on'];//返回日期
             $contractTeam['qty'] = $contract['traveler_num'];//旅游人数
             //行程安排
-            foreach ($contract['routes'] as $key=>$route){
+            foreach ($contract['routes'] as $key => $route) {
                 $contractTeam['routes'][$key]['day'] = $route['day'];//第几天行程
                 $contractTeam['routes'][$key]['stop'] = $route['stop'];//当天行程第几站
                 $contractTeam['routes'][$key]['departcity'] = $route['starting_address'];//出发地
                 $contractTeam['routes'][$key]['arrivecity'] = $route['destination_city'];//目的地城市
                 $contractTeam['routes'][$key]['arrivestate'] = $route['destination_state'];//前往省
                 $contractTeam['routes'][$key]['arrivenation'] = $route['destination_country'];//前往国家
-                $contractTeam['routes'][$key]['trip'] =  $route['trip'];//游览行程
+                $contractTeam['routes'][$key]['trip'] = $route['trip'];//游览行程
             }
 
             //游客名单
-            foreach ($contract['guests'] as $key=>$guest){
+            foreach ($contract['guests'] as $key => $guest) {
                 $contractTeam['guests'][$key]['idtype'] = '1';//证件类型身份证号
                 $contractTeam['guests'][$key]['idcode'] = $guest['id_card'];//证件号
                 $contractTeam['guests'][$key]['name'] = $guest['name'];//姓名
@@ -115,7 +123,11 @@ class GoldPalm
             $submitRequest['contractTeam'] = $contractTeam;//合同团队
             $client = new \SoapClient($this->api);
             $param = array('token' => $this->token, 'submitContractRequest' => $submitRequest);
-            $res = $client->__call('submitContract', ['param' => $param]);
+            if ($is_upload) {
+                $res = $client->__call('uploadContract', ['param' => $param]);
+            } else {
+                $res = $client->__call('submitContract', ['param' => $param]);
+            }
             $result = json_decode($res->return);
             if ($result->result == 'success') {
                 return $result->contract;
@@ -128,18 +140,196 @@ class GoldPalm
     }
 
     /**
-     * 根据合同ID获取是否签名
-     * @param $contractid
-     * @return bool|\Exception|\SoapFault
+     * 提交已经上传的合同
+     * @param $id
+     * @param $no
+     * @return bool
      * @throws \Exception
      */
-    public function getSignCreate($contractid)
+    public function submitStatus($id, $no)
     {
 
         try {
             $client = new \SoapClient($this->api);
-            $param = array('token' => $this->token, 'contractid' => $contractid);
+            $param = array('token' => $this->token, 'submitStatusRequest' => ['id' => $id, 'no' => $no]);
+            $res = $client->__call('submitStatus', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 取消合同
+     * @param $id
+     * @param $no
+     * @return bool
+     * @throws \Exception
+     */
+    public function cancelContract($id, $no)
+    {
+
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'cancelContractRequest' => ['id' => $id, 'no' => $no]);
+            $res = $client->__call('cancelContract', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 补充完善合同保险信息接口
+     * @param $no
+     * @param $insurance string json
+     * @return bool
+     * @throws \Exception
+     */
+    public function complementInsurance($no, $insurance)
+    {
+
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'complementInsuranceRequest' => ['no' => $no, 'insurance' => $insurance]);
+            $res = $client->__call('complementInsurance', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 补充游客列表只有为提交的合同可以使用
+     * @param $guests
+     * @return bool
+     * @throws \Exception
+     */
+    public function complementGuest($guests)
+    {
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'complementGuestRequest' =>$guests);
+            $res = $client->__call('complementGuest', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 补充签名功能
+     * @param $id
+     * @param $base64image
+     * @return bool
+     * @throws \Exception
+     */
+    public function submitSign($id,$base64image)
+    {
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'complementSignRequest' =>['id'=>$id,'base64image'=>$base64image]);
+            $res = $client->__call('submitSign', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+    /**
+     * 根据合同ID获取是否签名
+     * @param $id
+     * @return bool|\Exception|\SoapFault
+     * @throws \Exception
+     */
+    public function getSignCreate($id)
+    {
+
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'contractid' => $id);
             $res = $client->__call('getSignCreate', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 根据合同号获取合同基本信息
+     * @param $no
+     * @return bool
+     * @throws \Exception
+     */
+    public function getContractBaseInfo($no)
+    {
+
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'contractno' => $no);
+            $res = $client->__call('getContractBaseInfo', array('param' => $param));
+            $result = json_decode($res->return);
+
+            if ($result->result == 'success') {
+                return true;
+            } else {
+                throw  new \Exception(json_encode($result->errors));
+            }
+        } catch (\SoapFault $soapFault) {
+            throw  new \Exception($soapFault);
+        }
+    }
+
+    /**
+     * 重发短信
+     * @param $id
+     * @param $guestname
+     * @param $guestmobile
+     * @return bool
+     * @throws \Exception
+     */
+    public function resendMsg($id,$guestname,$guestmobile)
+    {
+
+        try {
+            $client = new \SoapClient($this->api);
+            $param = array('token' => $this->token, 'contractid' => $id,'guestname'=>$guestname,'guestmobile'=>$guestmobile);
+            $res = $client->__call('resendMsg', array('param' => $param));
             $result = json_decode($res->return);
 
             if ($result->result == 'success') {
